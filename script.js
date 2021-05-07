dcf8d1f1609ea24fc32a4e0efb2d95f7ea03c045
DavidHart05 <>
2021-05-07 14:34:37 +0000

    format

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ let userGuess
 document.getElementById('button').addEventListener('click', randomizer)
 
 // takes user input and converts to an integer
-function randomizer() {
+function randomizer () {
   userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-// generates a random number and converts to an integer
+  // generates a random number and converts to an integer
   randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
 
-  if (userGuess == randomNumber) {
-    document.getElementById('result').innerHTML = "You guessed the number!"
-  }
-  else {
-    document.getElementById('result').innerHTML = "Do you want to try again?"
+  if (userGuess === randomNumber) {
+    document.getElementById('result').innerHTML = 'You guessed the number!'
+  } else {
+    document.getElementById('result').innerHTML = 'Do you want to try again?'
   }
 }
